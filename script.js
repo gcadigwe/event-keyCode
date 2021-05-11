98e70b5c0db0c12494b16f57919bc09b6808ff6d
Sarzy <adigwegodswillchinonso@gmail.com>
2021-05-11 23:23:03 +0100

    javascript done

---
+++ script.js
@@ -0,0 +1,20 @@
+const inserts = document.getElementById("insert");
+
+window.addEventListener("keydown", (e) => {
+  inserts.innerHTML = `
+  <div class="key">
+  ${e.key === " " ? "Space" : e.key}
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${e.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${e.code}
+  <small>event.code</small>
+</div>
+    `;
+});
